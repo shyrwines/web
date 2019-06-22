884170a6cbc7f27197875ff22e25d000b39624cf
Yash Shah <yashshah0127@gmail.com>
2019-06-21 20:50:54 -0700

    Use platform agnostic home directory

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const fs = require('fs')
-
+const homedir = require('os').homedir()
 const app = express()
 
 app.get('/images/*', (req, res) => {
@@ -8,11 +7,11 @@ app.get('/images/*', (req, res) => {
 })
 
 app.get('/wines.json', (req, res) => {
-  res.sendFile('/Users/yash/Dropbox/Shyr/cli/wines.json')
+  res.sendFile(homedir + '/Dropbox/Shyr/cli/wines.json')
 })
 
 app.get('/wine-images/*', (req, res) => {
-  res.sendFile('/Users/yash/Dropbox/Shyr' + req.path)
+  res.sendFile(homedir + '/Dropbox/Shyr' + req.path)
 })
 
 app.listen(5000, () => console.log('Listening on port 5000'))
